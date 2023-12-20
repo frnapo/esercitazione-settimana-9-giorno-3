9ed24afcc32ea3a4460703d31bc6eca66fbbf3cf
Francesco <napo.fra96@gmail.com>
2023-12-20 16:46:42 +0100

    update2
    update2

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,11 +1,23 @@
+import React, { useState } from "react";
 import SingleBook from "./SingleBook";
 
-const BookList = ({ books }) => (
-  <div>
-    {books.map((book) => (
-      <SingleBook key={book.asin} book={book} />
-    ))}
-  </div>
-);
+const BookList = ({ books }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  return (
+    <>
+      <input className="m-3" type="text" placeholder="Cerca un libro" value={searchTerm} onChange={handleInputChange} />
+      {books
+        .filter((book) => book.title.toLowerCase().includes(searchTerm.toLowerCase()))
+        .map((book) => (
+          <SingleBook key={book.asin} book={book} />
+        ))}
+    </>
+  );
+};
 
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -2,7 +2,7 @@ import { Card } from "react-bootstrap";
 
 const SingleBook = ({ book }) => (
   <Card className="h-100 shadow border-0 d-flex flex-column">
-    <Card.Img variant="top" src={book.img} style={{ objectFit: "contain", height: "250px" }} />
+    <Card.Img variant="top" src={book.img} style={{ objectFit: "contain", height: "250px", cursor: "pointer" }} />
     <Card.Body className="d-flex flex-column justify-content-between">
       <Card.Title>{book.title}</Card.Title>
     </Card.Body>
